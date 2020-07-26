e71965da1a733503134bb437aca794fa5aa2f0a3
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-07-25 20:57:47 -0400

    feat: add route users

--- src/app.js
+++ src/app.js
@@ -4,4 +4,9 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+  const users = [{ name: 'Marcos', mail: 'marcos@mail.com' }];
+  res.status(200).json(users);
+});
+
 module.exports = app;
