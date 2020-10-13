6309bd7827fbf7271bcd55991cb3180b2a6580d3
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-12 23:00:54 -0400

    test: nao deve inserir usuario sem email

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -25,3 +25,21 @@ test('Deve inserir usuario com sucesso', () => {
       expect(res.body.name).toBe('Marcos Ramalho');
     });
 });
+
+test('Nao deve inserir usuario sem nome', () => {
+  return request(app)
+    .post('/users')
+    .send({ email: 'mmm@email.com', password: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Nome é um atributo obrigatório');
+    });
+});
+
+test('Nao deve inserir usuario sem email', async () => {
+  const result = await request(app)
+    .post('/users')
+    .send({ name: 'Maria', password: '123456' });
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe('Email é um atributo obrigatório');
+});
