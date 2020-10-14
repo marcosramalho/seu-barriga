1fe8e3f6fc7acd699e61877d98c0a2322dc55391
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-14 14:47:23 -0400

    feat: nao deve inserir uuma conta sem nome

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
   const create = (req, res) => {
     app.services.accounts.save(req.body).then((result) => {
+      if (result.error) return res.status(400).json(result);
       return res.status(201).json(result[0]);
     });
   };
