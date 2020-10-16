df7af151fe6e4a577000e7e4993ad19be1b4ced8
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-15 22:23:01 -0400

    feat: add exception erros application all

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,37 +1,39 @@
 module.exports = (app) => {
-  const create = (req, res) => {
+  const create = (req, res, next) => {
     app.services.accounts
       .save(req.body)
       .then((result) => {
         return res.status(201).json(result[0]);
       })
-      .catch((error) => {
-        return res.status(400).json({ error: error.message });
-      });
+      .catch((error) => next(error));
   };
 
-  const findAll = (req, res) => {
+  const findAll = (req, res, next) => {
     app.services.accounts
       .findAll()
-      .then((result) => res.status(200).json(result));
+      .then((result) => res.status(200).json(result))
+      .catch((error) => next(error));
   };
 
-  const findOne = (req, res) => {
+  const findOne = (req, res, next) => {
     app.services.accounts
       .findOne({ id: req.params.id })
-      .then((result) => res.status(200).json(result));
+      .then((result) => res.status(200).json(result))
+      .catch((error) => next(error));
   };
 
-  const update = (req, res) => {
+  const update = (req, res, next) => {
     app.services.accounts
       .update(req.params.id, req.body)
-      .then((result) => res.status(200).json(result[0]));
+      .then((result) => res.status(200).json(result[0]))
+      .catch((error) => next(error));
   };
 
-  const remove = (req, res) => {
+  const remove = (req, res, next) => {
     app.services.accounts
       .remove(req.params.id)
-      .then(() => res.status(204).send());
+      .then(() => res.status(204).send())
+      .catch((error) => next(error));
   };
 
   return {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,17 @@
 module.exports = (app) => {
-  const findAll = (req, res) => {
-    app.services.users.findAll().then((result) => res.status(200).json(result));
+  const findAll = (req, res, next) => {
+    app.services.users
+      .findAll()
+      .then((result) => res.status(200).json(result))
+      .catch((error) => next(error));
   };
 
-  const create = async (req, res) => {
+  const create = async (req, res, next) => {
     try {
       const result = await app.services.users.save(req.body);
       return res.status(201).json(result[0]);
     } catch (error) {
-      return res.status(400).json({ error: error.message });
+      return next(error);
     }
   };
 
